fbd504cd8a09d99a0cedeffb45e4d2ac143b616e
Arialdo Martini <arialdomartini@gmail.com>
2020-02-23 18:35:41 +0100

    Fix script error due to hugo escaping scripts

---
+++ js/registration.js
@@ -0,0 +1,13 @@
+(function ($) {
+  window.fnames = new Array();
+  window.ftypes = new Array();
+  fnames[0] = 'EMAIL';
+  ftypes[0] = 'email';
+  fnames[1] = 'FNAME';
+  ftypes[1] = 'text';
+  fnames[2] = 'LNAME';
+  ftypes[2] = 'text';
+  fnames[4] = 'MMERGE4';
+  ftypes[4] = 'radio';
+}(jQuery));
+var $mcj = jQuery.noConflict(true);
