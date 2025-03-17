8794a84cc6d2181805b5b4c5e0ba7d05a1655ff0
Matteo Meneguzzi <matteo.dev.meneguzzi@gmail.com>
2025-03-17 15:40:24 +0100

    chore: first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()],
 
   vite: {
     plugins: [tailwindcss()]
---
+++ src/components/utils.ts
@@ -0,0 +1,1 @@
+import Welcome from '../components/Welcome.astro';
