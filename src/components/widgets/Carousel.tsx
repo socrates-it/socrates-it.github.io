9a39e4777013745bb242a5ad5b46074b5ff7d8c8
Matteo Meneguzzi <matteo.dev.meneguzzi@gmail.com>
2025-03-21 17:09:50 +0100

    feat: added CTA component to every hero image

--- src/components/widgets/Carousel.tsx
+++ src/components/widgets/Carousel.tsx
@@ -3,11 +3,22 @@ import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import {cn} from "~/common/css.ts";
 
-interface CarouselProps {
-    images: { src: string; alt: string }[];
-}
+const images = [
+    {
+        'src': "src/assets/images/canyon.jpg",
+        'alt': 'canyon.jpg',
+    },
+    {
+        'src': "src/assets/images/fiori.jpg",
+        'alt': 'fiori.jpg',
+    },
+    {
+        'src': "src/assets/images/foresta.jpg",
+        'alt': 'foresta.jpg',
+    }
+]
 
-const Carousel = ({ images }: CarouselProps) => {
+const Carousel = () => {
     const [imageIndex, setImageIndex] = useState(0);
 
     const nextSlide = () => {
@@ -24,43 +35,42 @@ const Carousel = ({ images }: CarouselProps) => {
         });
     };
 
-    console.log(images)
-    //immagine 1: onNext: aggiungo transalte-x-full, onPRev: aggiungo -translate-x-full
-    //immagine 2: onNext: aggiungo transalte-x-full, onPRev: aggiungo -translate-x-full
-
     return (
-        <div className="relative w-full h-full">
+        <div className="group relative topw-full h-full">
             <div
-                className='w-full h-full flex overflow-hidden'
+                className='w-full h-full flex overflow-x-hidden'
             >
                 {images.map(({ src, alt }, index) => (
-                    <Fragment>
+                    <Fragment key={alt} >
                         <div
-                            className='object-cover w-full h-full block shrink-0 grow-0 transition-transform duration-1000 ease-in-out'
-                            style={{translate: `${-100 * imageIndex}%`}}
+                            className={`w-full h-full shrink-0 grow-0 transition-transform duration-1000 ease-in-out]`}
+                            style={{
+                                translate: `${-100 * imageIndex}%`,
+                                backgroundImage: `url(${src})`,
+                                backgroundPosition: 'center', // Adjust this value as needed
+                                backgroundSize: 'cover'
+                            }}
                         >
-                            <img
-                                key={src}
-                                src={src}
-                                alt={alt}
-                                aria-hidden={imageIndex !== index}
-                                className='object-cover w-full h-full block shrink-0 grow-0'
-                            />
-                            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl text-blue-500 flex space-x-8">
-                                <button className='bg-blue-900'>
-                                    get tickets
-                                </button>
-                                <button className='bg-blue-900'>
-                                    explore
-                                </button>
+                            <div className={cn(
+                                     "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2",
+                                     "text-center bg-black/50 p-6 rounded-xl text-white uppercase",
+                                     'space-y-10',
+                                     'opacity-0 group-hover:opacity-100 transition-opacity duration-1000',
+                                 )}
+                            >
+                                <p>Design Thinking Conference</p>
+                                <p>2025 edition Coming soon</p>
+                                <div className='flex justify-between items-center space-x-20'>
+                                    <button onClick={() => prevSlide()} className='py-2 px-6 bg-pink-700 rounded uppercase cursor-pointer'>get ticket</button>
+                                    <button onClick={() => nextSlide()} className='py-2 px-6 bg-pink-700 rounded uppercase cursor-pointer'>explore</button>
+                                </div>
                             </div>
                         </div>
                     </Fragment>
-
                 ))}
             </div>
             <button
-                className="absolute block top-1/2 left-5 transform -translate-y-1/2 bg-gray-800 transition-colors duration-300 hover:bg-green-950 text-white p-4 rounded z-10 cursor-pointer"
+                className="opacity-0 group-hover:opacity-100 absolute block top-1/2 left-5 transform -translate-y-1/2 bg-gray-800 transition-all duration-300 hover:bg-green-950 text-white p-4 rounded z-10 cursor-pointer"
                 onClick={prevSlide}
             >
                 <div
@@ -70,7 +80,7 @@ const Carousel = ({ images }: CarouselProps) => {
                 />
             </button>
             <button
-                className="absolute block top-1/2 right-5 transform -translate-y-1/2 bg-gray-800 transition-colors duration-300 hover:bg-green-950 text-white p-4 rounded z-10 cursor-pointer"
+                className="opacity-0 group-hover:opacity-100 absolute top-1/2 right-5 transform -translate-y-1/2 bg-gray-800 transition-all duration-300 hover:bg-green-950 text-white p-4 rounded z-10 cursor-pointer"
                 onClick={nextSlide}
             >
                 <div
@@ -80,12 +90,15 @@ const Carousel = ({ images }: CarouselProps) => {
                 />
             </button>
             <div
-                className='absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-3 bg-gray-800 p-2 rounded-xl z-50'
+                className={
+                    cn('absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-3 bg-gray-800 p-2 rounded-xl z-50',
+                    'opacity-0 group-hover:opacity-100 transition-opacity duration-300')
+                }
             >
                 {images.map((_, index) => (
                     <button
                         key={index}
-                        className="[unset:all] block cursor-pointer w-2.5 h-2.5  relative"
+                        className="[unset:all] block cursor-pointer w-2.5 h-2.5 relative"
                         aria-label={`View Image ${index + 1}`}
                         onClick={() => setImageIndex(index)}
                     >
