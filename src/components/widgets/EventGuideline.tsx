d5f4d991d94a7e1d987098206d7abbfbfdd8c6bb
Matteo Meneguzzi <matteo.dev.meneguzzi@gmail.com>
2025-04-01 10:31:06 +0200

    feat: added event guideline

---
+++ src/components/widgets/EventGuideline.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import SectionIntro from '~components/ui/SectionIntro.tsx'
+
+const EventGuideline = () => {
+  return (
+    <section className="bg-page text-default p-container">
+      <SectionIntro title={'Event Guideline'} description={'Global Grand Event on Digital Design'} />
+
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center mt-12">
+        <div className="md:w-1/2 p-4">
+          <img src="src/assets/images/canyon.jpg" alt="Event Presentation" className="rounded-lg shadow-lg" />
+        </div>
+
+        <div className="md:w-1/2 p-4">
+          <p className="text-gray-300 mb-6">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
+            1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+          </p>
+
+          <h3 className="text-xl font-semibold mb-4">Check List</h3>
+          <ul className="text-gray-300 space-y-2">
+            <li className="flex items-center">
+              <span className="text-green-400 mr-2">✔</span> Lorem Ipsum is simply dummy
+            </li>
+            <li className="flex items-center">
+              <span className="text-green-400 mr-2">✔</span> Ipsum passages, and more recently
+            </li>
+            <li className="flex items-center">
+              <span className="text-green-400 mr-2">✔</span> PageMaker including versions
+            </li>
+            <li className="flex items-center">
+              <span className="text-green-400 mr-2">✔</span> Lorem Ipsum is simply dummy
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default EventGuideline
