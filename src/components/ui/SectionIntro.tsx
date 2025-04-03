a5550cf113677f740c14fa0c96268442efc06318
Matteo Meneguzzi <matteo.dev.meneguzzi@gmail.com>
2025-04-03 18:08:34 +0200

    refactor: fixed dark mode accordion

--- src/components/ui/SectionIntro.tsx
+++ src/components/ui/SectionIntro.tsx
@@ -5,8 +5,8 @@ interface SectionIntroProps {
 
 const SectionIntro = ({ title, description }: SectionIntroProps) => {
   return (
-    <div className="text-default pb-xl text-center">
-      <h2 className="text-4xl md:text-5xl font-bold text-heading uppercase">{title}</h2>
+    <div className="text-default dark:text-light pb-xl text-center">
+      <h2 className="text-4xl md:text-5xl font-bold text-heading dark:text-light uppercase">{title}</h2>
       <div className="mt-2 flex justify-center">
         <div className="w-12 h-[2px] bg-primary translate-x-1 translate-y-0.5"></div>
         <div className="w-12 h-[2px] bg-primary -translate-x-1 -translate-y-0.5"></div>
--- src/components/widgets/Accordion.tsx
+++ src/components/widgets/Accordion.tsx
@@ -24,35 +24,35 @@ export default function Accordion() {
           <div key={index} className="shadow-lg group">
             <button
               className={cn(
-                'h-20 w-full text-heading text-lg font-semibold p-4 cursor-pointer ',
-                'border border-primary group-first:rounded-t-2xl transition-colors duration-300',
+                'h-20 w-full text-heading dark:text-light-contrast text-lg font-semibold p-4 cursor-pointer ',
+                'border border-primary dark:border-light-contrast group-first:rounded-t-2xl transition-colors duration-300',
                 'flex justify-between items-center ',
-                'bg-highlight',
-                { 'border-b-primary': openIndex === index, 'border-b-transparent': openIndex !== index },
+                'bg-highlight dark:bg-contrast',
+                { 'border-b-primary dark:border-b-light-contrast': openIndex === index, 'border-b-transparent dark:border-b-transparent': openIndex !== index },
               )}
               onClick={() => toggleAccordion(index)}>
               <div className="flex items-center justify-between">
-                <div className="mr-2">{item.icon}</div>
+                <div className="mr-2 text-primary-dark dark:text-secondary">{item.icon}</div>
                 {item.title}
               </div>
-              <div className="size-4  flex items-center justify-between">
-                <div className="w-full h-0.5 bg-default" />
+              <div className="size-4 flex items-center justify-between">
+                <div className="w-full h-0.5 bg-default dark:bg-light-contrast" />
                 <div
-                  className={`w-0.5 h-full bg-default -translate-x-2 transition-opacity duration-75 ${openIndex !== index ? 'opacity-100' : 'opacity-0 '}`}
+                  className={`w-0.5 h-full bg-default dark:bg-light-contrast -translate-x-2 transition-opacity duration-75 ${openIndex !== index ? 'opacity-100' : 'opacity-0 '}`}
                 />
               </div>
             </button>
             <div
               className={cn(
-                'border-x border-x-primary group-last:border-b group-last:border-b-primary',
-                'transition-[height] overflow-hidden text-default duration-300',
+                'border-x border-x-primary dark:border-x-light-contrast group-last:border-b group-last:border-b-primary dark:group-last:border-b-light-contrast',
+                'transition-[height] overflow-hidden duration-300',
                 'flex justify-between items-center ',
-                'bg-light border-t-0',
+                'bg-light dark:bg-dark border-t-0',
                 { 'h-20 ': openIndex === index, 'h-0': openIndex !== index },
               )}>
               <p
                 className={cn('relative pl-4 transition-colors duration-100', {
-                  'text-default': openIndex === index,
+                  'text-default dark:text-white': openIndex === index,
                   'text-transparent': openIndex !== index,
                 })}>
                 {item.content}
