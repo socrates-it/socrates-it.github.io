1eee354578f4f4e6a7e6c359e1d3517f56f127f0
Matteo Meneguzzi <matteo.dev.meneguzzi@gmail.com>
2025-04-03 15:13:51 +0200

    chore: fixed navigation style

--- src/components/navbar/MobileMenu.tsx
+++ src/components/navbar/MobileMenu.tsx
@@ -12,7 +12,7 @@ type HProps = {
 
 const Hamburger: FC<HProps> = ({ isOpen, onClose, onOpen }) => {
   return (
-    <div className="xp-4 space-y-1.5 cursor-pointer w-6 h-6" onClick={isOpen ? onClose : onOpen}>
+    <div className="xp-4 space-y-1.5 cursor-pointer size-6 translate-y-1" onClick={isOpen ? onClose : onOpen}>
       <span className={`block w-6 h-0.5 bg-page transition-all duration-300 ease-in-out ${isOpen ? 'opacity-0' : ''}`}></span>
       <span
         className={`block w-4 h-0.5 bg-page transition-all duration-300 ease-in-out ${isOpen ? 'transform rotate-45 absolute w-6' : 'animate-spin'}`}></span>
@@ -40,15 +40,15 @@ const MobileMenu: FC<Props> = ({ items }) => {
     <div className="text-page">
       <Hamburger isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
       <div
-        className={cn('w-full mx-auto px-5', 'absolute left-0 right-0 top-17', 'bg-primary', 'z-10 overflow-hidden transition-[height] duration-700', {
-          'h-21': isOpen,
+        className={cn('w-full mx-auto px-5', 'absolute left-0 right-0 top-15', 'bg-primary', 'z-10 overflow-hidden transition-[height] duration-700', {
+          'h-31': isOpen,
           'h-0': !isOpen,
         })}>
         <div className="z-10 ">
-          <ul data-open={isOpen} className={` border-t ${isOpen ? 'border-t-page' : 'border-t-transparent'} transition-colors duration-700`}>
+          <ul data-open={isOpen} className={` border-t ${isOpen ? 'border-t-page' : 'border-t-transparent'} transition-colors duration-700 py-2`}>
             {items.map(x => {
               return (
-                <li key={x.label} className="text-xl border-l-6 pl-2 border-transparent hover:border-accent transition-colors duration-300">
+                <li key={x.label} className="text-xl border-l-6 pl-2 py-1 border-transparent hover:border-accent transition-colors duration-300">
                   <MenuLink item={x} />
                 </li>
               )
