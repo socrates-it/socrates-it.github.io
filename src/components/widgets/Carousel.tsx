3d0e4e0610d53bc41fe8185d4316c54d1e2baed9
Matteo Meneguzzi <matteo.dev.meneguzzi@gmail.com>
2025-04-14 15:38:43 +0200

    style: restyled nav and carousel

--- src/components/widgets/Carousel.tsx
+++ src/components/widgets/Carousel.tsx
@@ -50,41 +50,39 @@ const Carousel = () => {
               }}>
               <div
                 className={cn(
-                  'absolute -bottom-8 left-1/2 transform -translate-x-1/2 -translate-y-1/2',
-                  'text-center rounded-xl',
-                  'space-y-4 sm:space-y-6 w-full text-xs sm:text-lg',
+                  'absolute bg-light/80 dark:bg-contrast -bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2',
+                  'text-center',
+                  'space-y-4 sm:space-y-6 w-full text-lg md:text-2xl',
                   'opacity-100',
                 )}>
-                <div className="bg-primary space-y-8">
-                  <div className="w-50 sm:w-100 mx-auto bg-light dark:bg-contrast dark:text-light rounded-xl space-y-2 font-bold p-4">
-                    <p className="font-bold">SoCraTes 2025 edition coming soon</p>
-                  </div>
-                  {/*<div className="flex justify-between items-center space-x-4 mx-auto w-50 sm:w-75 h-lg">*/}
-                  {/*  <button onClick={() => prevSlide()} className="btn-primary text-xs h-full w-30 rounded-lg">*/}
-                  {/*    get ticket*/}
-                  {/*  </button>*/}
-                  {/*  <button onClick={() => nextSlide()} className="btn-primary text-xs h-full w-30 rounded-lg">*/}
-                  {/*    explore*/}
-                  {/*  </button>*/}
-                  {/*</div>*/}
+                <div className="w-80 sm:w-100 mx-auto text-primary rounded-xl p-1">
+                  <p className="italic">SoCraTes 2025 edition coming soon</p>
                 </div>
+                {/*<div className="flex justify-between items-center space-x-4 mx-auto w-50 sm:w-75 h-lg">*/}
+                {/*  <button onClick={() => prevSlide()} className="btn-primary text-xs h-full w-30 rounded-lg">*/}
+                {/*    get ticket*/}
+                {/*  </button>*/}
+                {/*  <button onClick={() => nextSlide()} className="btn-primary text-xs h-full w-30 rounded-lg">*/}
+                {/*    explore*/}
+                {/*  </button>*/}
+                {/*</div>*/}
               </div>
             </div>
           </Fragment>
         ))}
       </div>
       <div className="relative bg-transparent px-custom">
-        <div className="container-custom flex justify-between items-center">
+        <div className="max-w-6xl mx-auto flex justify-between items-center">
           <button
             className={cn(
               'opacity-100',
               'bg-primary/20 border border-secondary transition-all duration-300 hover:bg-primary-dark/40',
-              'p-2 sm:p-4 rounded z-10 cursor-pointer relative bottom-57.5',
+              'p-2 md:p-4 rounded z-10 cursor-pointer relative bottom-50 -translate-y-14 md:-translate-y-40',
             )}
             onClick={prevSlide}>
             <div
-              className="h-3 w-3 sm:w-6 sm:h-6 border-secondary
-                  border-l-2 border-b-2 translate-x-0.5 sm:translate-x-1.5
+              className="h-3 w-3 md:w-6 md:h-6 border-secondary
+                  border-l-2 border-b-2 translate-x-0.5 md:translate-x-1.5
                   transform rotate-45 rounded-bl"
             />
           </button>
@@ -93,36 +91,36 @@ const Carousel = () => {
             className={cn(
               'opacity-100',
               'bg-primary/20 border border-secondary transition-all duration-300 hover:bg-primary-dark/40',
-              'p-2 sm:p-4 rounded z-10 cursor-pointer relative bottom-57.5',
+              'p-2 md:p-4 rounded z-10 cursor-pointer relative bottom-50 -translate-y-14 md:-translate-y-40',
             )}
             onClick={nextSlide}>
             <div
-              className="h-3 w-3 sm:w-6 sm:h-6 border-secondary
-                  border-t-2 border-r-2 -translate-x-0.5 sm:-translate-x-1.5
+              className="h-3 w-3 md:w-6 md:h-6 border-secondary
+                  border-t-2 border-r-2 -translate-x-0.5 md:-translate-x-1.5
                   transform rotate-45 rounded-tr"
             />
           </button>
         </div>
       </div>
-      <div
-        className={cn(
-          'absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-3 bg-gray-800 p-2 rounded-xl',
-          'opacity-100 sm:opacity-0 group-hover:opacity-100 transition-opacity duration-300',
-        )}>
-        {images.map((_, index) => (
-          <button
-            key={index}
-            className="[unset:all] block cursor-pointer w-2.5 h-2.5 relative"
-            aria-label={`View Image ${index + 1}`}
-            onClick={() => setImageIndex(index)}>
-            {index === imageIndex ? (
-              <div className="w-full h-full bg-subtitle rounded-full"></div>
-            ) : (
-              <div className="w-full h-full border-1 transition-all hover:scale-90 duration-300 hover:bg-primary border-light-contrast rounded-full"></div>
-            )}
-          </button>
-        ))}
-      </div>
+      {/*<div*/}
+      {/*  className={cn(*/}
+      {/*    'absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-3 bg-gray-800 p-2 rounded-xl',*/}
+      {/*    'opacity-100 sm:opacity-0 group-hover:opacity-100 transition-opacity duration-300',*/}
+      {/*  )}>*/}
+      {/*  {images.map((_, index) => (*/}
+      {/*    <button*/}
+      {/*      key={index}*/}
+      {/*      className="[unset:all] block cursor-pointer w-2.5 h-2.5 relative"*/}
+      {/*      aria-label={`View Image ${index + 1}`}*/}
+      {/*      onClick={() => setImageIndex(index)}>*/}
+      {/*      {index === imageIndex ? (*/}
+      {/*        <div className="w-full h-full bg-subtitle rounded-full"></div>*/}
+      {/*      ) : (*/}
+      {/*        <div className="w-full h-full border-1 transition-all hover:scale-90 duration-300 hover:bg-primary border-light-contrast rounded-full"></div>*/}
+      {/*      )}*/}
+      {/*    </button>*/}
+      {/*  ))}*/}
+      {/*</div>*/}
     </div>
   )
 }
