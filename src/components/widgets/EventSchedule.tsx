9c67a8813e0e82453f6f13d9d88af99befe52202
Matteo Meneguzzi <matteo.dev.meneguzzi@gmail.com>
2025-04-17 18:47:08 +0200

    fix: added correct content to EventSchedule

--- src/components/widgets/EventSchedule.tsx
+++ src/components/widgets/EventSchedule.tsx
@@ -1,5 +1,15 @@
-import React, { useState } from 'react'
+import React, { type JSX, useState } from 'react'
 import SectionIntro from '~components/ui/SectionIntro.tsx'
+import {
+  FluentConferenceRoom20Regular,
+  FluentDrinkMargarita20Regular,
+  FluentMdl2CoffeeScript,
+  FluentMdl2EatDrink,
+  FluentMdl2Group,
+  FluentMdl2Lightbulb,
+  FluentMdl2WavingHand,
+  FluentPersonVoice16Regular,
+} from '~/assets/icons/Icons.tsx'
 
 const eventData: Record<
   string,
@@ -7,44 +17,121 @@ const eventData: Record<
     time: string
     title: string
     description: string
+    icon: JSX.Element
   }[]
 > = {
   'October 2': [
     {
-      time: '3:00 pm',
-      title: 'Welcome In!',
+      time: '3:00pm - 7:30pm',
+      title: 'Welcome in!',
       description: 'We kick off SoCraTes IT with a good beer and networking activities',
+      icon: <FluentMdl2WavingHand className="size-6" />,
+    },
+    {
+      time: '7:30pm - 9:00pm',
+      title: 'Dinner time',
+      description: 'We have a good dinner all together',
+      icon: <FluentMdl2EatDrink className="size-6" />,
+    },
+    {
+      time: '9:00pm - open end',
+      title: 'After dinner',
+      description: 'We do a team-building game to know each others',
+      icon: <FluentDrinkMargarita20Regular className="size-6" />,
     },
   ],
   'October 3': [
     {
-      time: '9am - 11:30am',
-      title: 'Advanced UI/UX Design Techniques',
-      description: 'Exploring the latest trends in UI/UX design with a hands-on approach...',
+      time: '8:00am - 9:00am',
+      title: 'Wake up, lazy guys!',
+      description: 'Time to have a breakfast',
+      icon: <FluentMdl2CoffeeScript className="size-6" />,
     },
     {
-      time: '2pm - 4pm',
-      title: 'Typography in Digital Media',
-      description: 'How typography impacts user experience and readability...',
+      time: '9:00am - 9:30am',
+      title: 'Official opening',
+      description: 'Introduction to the spirit of SoCraTes IT and the Open Space',
+      icon: <FluentConferenceRoom20Regular className="size-6" />,
+    },
+    {
+      time: '9:30am - 10:00am',
+      title: 'Marketplace',
+      description: 'Time for everyone to write down their proposals and pick a slot',
+      icon: <FluentMdl2Lightbulb className="size-6" />,
+    },
+    {
+      time: '10:00am - 1:00pm',
+      title: 'Sessions',
+      description: '1 hour sessions',
+      icon: <FluentMdl2Group className="size-6" />,
+    },
+    {
+      time: '1:00pm - 2:00pm',
+      title: 'Lunch',
+      description: 'Time to refuel',
+      icon: <FluentMdl2EatDrink className="size-6" />,
+    },
+    {
+      time: '2:00pm - 5:30pm',
+      title: 'Sessions',
+      description: '1 hour sessions',
+      icon: <FluentMdl2Group className="size-6" />,
+    },
+    {
+      time: '5:30pm - 7:30pm',
+      title: 'Evening news',
+      description: 'We recap the first day and we share your impression',
+      icon: <FluentPersonVoice16Regular className="size-6" />,
+    },
+    {
+      time: '7:30pm - open end',
+      title: 'Dinner',
+      description: 'Time to give energy to your brain',
+      icon: <FluentMdl2EatDrink className="size-6" />,
     },
   ],
   'October 4': [
     {
-      time: '11am - 1pm',
-      title: 'Color Psychology in Branding',
-      description: 'Understanding the role of colors in brand perception...',
-    },
-  ],
-  'October 5': [
-    {
-      time: '10am - 12:30pm',
-      title: 'Web Design Principles and Best Practices',
-      description: 'Consectetur adipiscing elit. Quod distinctio impedit sint accusantium ducimus...',
+      time: '8:00am - 9:00am',
+      title: 'The early bird gets the worm!',
+      description: 'Time to have a breakfast',
+      icon: <FluentMdl2CoffeeScript className="size-6" />,
     },
     {
-      time: '2pm - 4pm',
-      title: 'Interactive Web Animations',
-      description: 'Using CSS & JS to create smooth, engaging animations for modern web apps...',
+      time: '9:00am - 10:00am',
+      title: 'Marketplace',
+      description: 'Time for everyone to write down their proposals and pick a slot',
+      icon: <FluentMdl2Lightbulb className="size-6" />,
+    },
+    {
+      time: '10:00am - 1:00pm',
+      title: 'Sessions',
+      description: '1 hour sessions',
+      icon: <FluentMdl2Group className="size-6" />,
+    },
+    {
+      time: '1:00pm - 2:00pm',
+      title: 'Lunch',
+      description: 'Time to refuel',
+      icon: <FluentMdl2EatDrink className="size-6" />,
+    },
+    {
+      time: '2:00pm - 4:00pm',
+      title: 'Sessions',
+      description: '1 hour sessions',
+      icon: <FluentMdl2Group className="size-6" />,
+    },
+    {
+      time: '4:00pm - 5:00pm',
+      title: 'Official closing',
+      description: 'We recap all the days and share our impression',
+      icon: <FluentPersonVoice16Regular className="size-6" />,
+    },
+    {
+      time: '7:30pm - open end',
+      title: 'Goodbye to the next edition!',
+      description: "Time flies when you're having fun",
+      icon: <FluentMdl2WavingHand className="size-6" />,
     },
   ],
 }
@@ -72,10 +159,10 @@ const EventSchedule = () => {
         {/* Event Details */}
         <div className="w-full sm:w-2/3">
           {eventData[selectedDate].map((event, index) => (
-            <div key={index} className="bg-light p-6 mb-4 rounded-lg shadow-lg transition-colors duration-300">
+            <div key={index} className="bg-light p-4 mb-4 rounded-lg shadow-lg transition-colors duration-300">
               <div className="flex items-center gap-4">
-                <div className="min-w-12 size-12 bg-gray-500 rounded-full"></div>
-                <div className="space-y-4 sm:space-y-2">
+                <div className="size-12 bg-primary text-light rounded-full flex justify-center items-center">{event.icon}</div>
+                <div className="space-y-0">
                   <p className="text-sm text-gray-400">{event.time}</p>
                   <h3 className="text-lg font-bold">{event.title}</h3>
                 </div>
