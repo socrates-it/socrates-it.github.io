756199358f05560b91afc1fab4669a0c973d7872
Andrea Vallotti <andrea.vallotti@gmail.com>
2026-07-29 15:14:49 +0200

    fix: remove rounding around sponsor logos

--- src/components/widgets/Sponsors.tsx
+++ src/components/widgets/Sponsors.tsx
@@ -114,7 +114,7 @@ function Logo(sponsor: Sponsor) {
   if (sponsor.type === 'rectangle')
     return (
       <div
-        className={cn('rounded-4xl h-16 sm:h-20 w-32 sm:w-40 bg-contain bg-center bg-no-repeat')}
+        className={cn('h-16 sm:h-20 w-32 sm:w-40 bg-contain bg-center bg-no-repeat')}
         style={{ backgroundImage: `url(${prependBasePath(sponsor.image)})` }}
       />
     )
